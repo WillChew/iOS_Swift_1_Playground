0545b8dcc8f499b8edfb6856239bf7ef3dfc4af5
WillChew <williamchew95@gmail.com>
2018-06-25 15:59:09 -0400

    finished loops (can't find smallest number

--- W4D1_Playground.playground/Pages/Loops.xcplaygroundpage/Contents.swift
+++ W4D1_Playground.playground/Pages/Loops.xcplaygroundpage/Contents.swift
@@ -29,24 +29,27 @@ for i in 2...5 {
  - Experiment:
  What's the difference between `2...5` vs `2..<5`?
  */
-
+//the first one goes up to 5, the 2nd one doesnt include 5 so it goes to 4
 
 /*:
  - Experiment:
   Use a ranged for loop to search through the 'pets' array above for the word 'pig' and print out its index.
  */
 
-
 /*:
  - Experiment:
  Create an array of random numbers of your choosing then make a for loop that adds 1 to each number.
  */
-
+let otherNumberArray = [1,2,3,4,5,6,7,8]
+for number in otherNumberArray {
+    print("\(number+1)")
+}
 
 /*:
  - Experiment:
  Take a look at the example below. Modify the code so that it searches for both the largest and smallest number. Then print out the result.
  */
+
 
 /*:
  Hint: Use an `_` so that you can avoid making another unecessary variable for the iteration value.
@@ -73,6 +76,16 @@ print(largest)
  Given a number `N`, from 0 to `N`, add up all the odd numbers and print out the result.
  ie: N = 5, 1+3+5 = 9
  */
+var oddNumber = 0
+var sum = 0
+
+for i in 0...5 {
+    if i % 2 != 0 {
+      oddNumber = i
+        sum += oddNumber
+    }
+}
+print("\(sum)")
 
 
 /*:
@@ -89,5 +102,27 @@ print(largest)
  */
 
 let numberArray = [1, 4, 5, 5, 5, 3, 2, 1, 4, 2, 2, 2, 1]
+var count1 = 0
+var count2 = 0
+var count3 = 0
+var count4 = 0
+var count5 = 0
 
+for number in numberArray {
+    switch number {
+    case 1 :
+        count1 += 1
+    case 2 :
+        count2 += 1
+    case 3 :
+        count3 += 1
+    case 4 :
+        count4 += 1
+    case 5 :
+        count5 += 1
+    default:
+        break
+    }
+}
+print("1: occurs \(count1) times, 2: occurs \(count2) times, 3: occurs \(count3) times, 4: occurs \(count4) times, 5: occurs \(count5) times")
 //: [Next](@next)
